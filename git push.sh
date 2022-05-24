94cdbfa4718348e640706f3217721213cefe6746
baixxq <baixxq@163.com>
2022-05-24 23:47:08 +0800

    2022-05-24

--- git push.sh
+++ git push.sh
@@ -1,6 +1,4 @@
 now=$(date "+%Y-%m-%d")
-echo "Change Directory to E:\Github\girlfriend-gift-collection"
-cd E:/Github/girlfriend-gift-collection
 echo "Starting add-commit-pull-push..."
 git add -A && git commit -m "$now" && git push
 echo "Finish!"
