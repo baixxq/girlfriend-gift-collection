ced4b47e1870c8321a99b9d916f76b798c873f1e
baixxqPro14 <baixxq@163.com>
2023-04-03 17:53:17 +0800

    2023-04-03

--- birth/config.js
+++ birth/config.js
@@ -31,11 +31,11 @@ var config = {
      */
     imgs: {
         "送给我": "./imgs/songxinxin.jpeg",
-        "心爱的姑娘": "./imgs/nothing.jpg",
+        "心爱的刘小姐": "./imgs/nothing.jpg",
         "今天是你的生日": "./imgs/shengri.jpeg",
-        "这是我们在一起的": "./imgs/zaiyiqi.png",
-        "第三个生日！": "./imgs/nothing.jpg",
-        "这两年来，我见过你": "./imgs/yangzi.jpeg",
+        "这是我们异地的": "./imgs/zaiyiqi.png",
+        "第三年！": "./imgs/nothing.jpg",
+        "这些年，我见过你": "./imgs/yangzi.jpeg",
         "奇怪的样子": "./imgs/nothing.jpg",
         "可爱的样子": "./imgs/nothing.jpg",
         "生气的样子": "./imgs/nothing.jpg",
@@ -43,9 +43,13 @@ var config = {
         "对了，礼物收全了吗？": "./imgs/chuo.gif",
         "今年要吃好好的": "./imgs/chihaodian.jpeg",
         "喂饱饱的": "./imgs/weibao.jpeg",
-        "然后才有力气干活": "./imgs/nothing.jpg",
+        "然后才有力气学习": "./imgs/nothing.jpg",
         "和想我！": "./imgs/nothing.jpg",
-        "生日快乐~~": "./imgs/nothing.jpg"
+        "我们马上结束异地啦": "./imgs/nothing.jpg",
+        "幸福的日子就要来啦": "./imgs/nothing.jpg",
+        "我们一起加油呀": "./imgs/nothing.jpg",
+        "生日快乐~~": "./imgs/nothing.jpg",
+        "橙子永远爱你呀": "./imgs/nothing.jpg"
     },
     
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
@@ -57,9 +61,9 @@ var config = {
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "生日快乐～",
-        story: "A MESSAGE FOR YOU"
+        story: "I MISS AND LOVE YOU"
     },
 
     // 结束语
-    loveText: '爱你的X先生~'
+    loveText: '爱你的橙子~'
 };
